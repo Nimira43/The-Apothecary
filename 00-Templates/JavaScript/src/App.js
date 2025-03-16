c18f4056e2a733900fe92fe8e74f1ead6e975c35
Nimira43 <n.rabone72@aol.com>
2025-03-16 18:28:01 +0000

    Brought in React starter templates

---
+++ 00-Templates/JavaScript/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 className='logo'>Artemis</h1>
+      <p>React 18 - Starter Boilerplate</p>
+      <p>Using Babel & Webpack</p>
+    </div>
+  )
+}
+
+export default App
