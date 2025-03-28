c6a9d2e2df649cf4d596b24883837016bc4c6f88
Nimira43 <n.rabone72@aol.com>
2025-03-28 16:07:37 +0000

    Created app component properties.

--- Angular/Simple-Profile-Card/src/app/app.component.ts
+++ Angular/Simple-Profile-Card/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core'
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+  name: string = 'Bob Smith'
+  email: string = 'bob@user.com'
+  message: string = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, aperiam.'
+}
