7e37f03d67cb8d292defe1b2340a25ca62763157
Nimira43 <n.rabone72@aol.com>
2025-03-30 13:47:03 +0100

    imports into RegistrationForm and App.

--- React/Registration-Form/src/App.jsx
+++ React/Registration-Form/src/App.jsx
@@ -1,7 +1,11 @@
+import React from 'react'
+
+import RegistrationForm from './components/RegistrationForm'
+
 function App() {
   return (
     <div className='App'>
-
+      <RegistrationForm />
     </div>
   )
 }
--- React/Registration-Form/src/components/RegistrationForm.jsx
+++ React/Registration-Form/src/components/RegistrationForm.jsx
@@ -1,3 +1,7 @@
+import React from 'react'
+import Title from './Title'
+import Button from './Button'
+
 export default function RegistrationForm() {
   return (
     <div>RegistrationForm</div>
