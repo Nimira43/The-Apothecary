eea17b267ac474ddb4776f873d031215fa71bc65
Nimira43 <n.rabone72@aol.com>
2025-04-08 17:17:04 +0100

    Creating the start of a template for Vanilla projects.

--- React/Registration-Form/src/components/RegistrationForm.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Title from './Title'
-import Button from './Button'
-
-export default function RegistrationForm() {
-  return (
-    <div>RegistrationForm</div>
-  )
-}
