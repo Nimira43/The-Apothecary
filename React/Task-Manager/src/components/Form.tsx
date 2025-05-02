01ff2d32c9fcdabe9355e67a535154e05cf3aa3d
Nimira43 <n.rabone72@aol.com>
2025-05-02 06:18:42 +0100

    Created handleSubmit function

--- React/Task-Manager/src/components/Form.tsx
+++ React/Task-Manager/src/components/Form.tsx
@@ -3,6 +3,16 @@ import React, { useState } from 'react'
 const Form = () => {
   const [text, setText] = useState('')
   
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    if (!text) {
+      alert('Please enter a Task')
+      return
+    }
+    setText('')
+  }
+  
   return (
     
   )
