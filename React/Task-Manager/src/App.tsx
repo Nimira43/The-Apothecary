6bef0b2b7be3804b43eb4d3b6b8aafe73f80c527
Nimira43 <n.rabone72@aol.com>
2025-05-02 06:09:27 +0100

    Import type Task

--- React/Task-Manager/src/App.tsx
+++ React/Task-Manager/src/App.tsx
@@ -3,6 +3,7 @@ import Form from './components/Form'
 import List from './components/List'
 import './index.css'
 import { useEffect, useState } from 'react'
+import { type Task } from './utils/types'
 
 
 function App() {
