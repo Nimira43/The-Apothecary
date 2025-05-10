4ed02b696f61bfc3b4486893984d35867f27ef25
Nimira43 <n.rabone72@aol.com>
2025-05-10 17:02:23 +0100

    Updated index.tsx

--- Redux/Task-Manager/src/index.tsx
+++ Redux/Task-Manager/src/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { Provider } from 'react-redux'
 import './index.css'
+import store from './store'
 
-const rootElement = document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement)
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  )
-} else {
-  console.error('Root element not found in the DOM.')
-}
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
