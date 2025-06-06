13950dcc64d1ee1fa3dfce8c643b407ddaf9127a
Nimira43 <n.rabone72@aol.com>
2025-06-06 20:43:22 +0100

    Setting up calendat days and dates. Hardcoded for the moment.

--- React/Calendar/src/components/Calender.jsx
+++ React/Calendar/src/components/Calender.jsx
@@ -5,14 +5,55 @@ const Calender = () => {
   return (
     <div className='calendar-app'>
       <div calendar>
-        <h1 className='heading text-5xl'>Calendar</h1>
+        <h1 className='heading text-4xl'>Calendar</h1>
         <div className='navigate-date'>
-          <h2 className='month text-3xl'>June</h2>
+          <h2 className='month text-3xl'>July</h2>
           <h2 className='year text-3xl'>2025</h2>
           <div className='buttons'>
-
+            <GrCaretPrevious />
+            <GrCaretNext />
           </div>
         </div>
+        <div className='weekdays'> 
+          <span>Sunday</span>
+          <span>Monday</span>
+          <span>Tuesday</span>
+          <span>Wednesday</span>
+          <span>Thursday</span>
+          <span>Friday</span>
+          <span>Saturday</span>
+        </div>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+        <span>5</span>
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+        <span>10</span>
+        <span>11</span>
+        <span>12</span>
+        <span>13</span>
+        <span>14</span>
+        <span>15</span>
+        <span>16</span>
+        <span>17</span>
+        <span>18</span>
+        <span>19</span>
+        <span>20</span>
+        <span>21</span>
+        <span>22</span>
+        <span>23</span>
+        <span>24</span>
+        <span>25</span>
+        <span>26</span>
+        <span>27</span>
+        <span>28</span>
+        <span>29</span>
+        <span>30</span>
+        <span>31</span>
       </div>
     </div>
   )
