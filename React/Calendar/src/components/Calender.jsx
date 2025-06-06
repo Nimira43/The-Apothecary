9ea80d0733acde34291f96c7dbf297de237d8c8a
Nimira43 <n.rabone72@aol.com>
2025-06-06 20:33:49 +0100

    Importing caret icons.

--- React/Calendar/src/components/Calender.jsx
+++ React/Calendar/src/components/Calender.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { GrCaretNext, GrCaretPrevious } from "react-icons/gr"
 
 const Calender = () => {
   return (
     <div className='calendar-app'>
       <div calendar>
-        <h1 className='heading'>Calendar</h1>
+        <h1 className='heading text-5xl'>Calendar</h1>
         <div className='navigate-date'>
-          <h2 className='month'>June</h2>
-          <h2 className='year'>2025</h2>
-          <div className='buttons'></div>
+          <h2 className='month text-3xl'>June</h2>
+          <h2 className='year text-3xl'>2025</h2>
+          <div className='buttons'>
+
+          </div>
         </div>
       </div>
     </div>
