cef3fdbcdc07f79f037676c8ed655092e497ab45
Nimira43 <n.rabone72@aol.com>
2025-09-10 04:32:39 +0100

    JS Cube - 1

--- CSS/Vanilla/3D-Cubes/script.js
+++ CSS/Vanilla/3D-Cubes/script.js
@@ -1,4 +1,12 @@
 function cubes() {
   let container = document.querySelector('.container')
   let zValues = [-3, -2, -1, 0, 1, 2, 3]
+
+  zValues.forEach(z => {
+    let cube = document.createElement('div')
+    cube.classList.add('cube')
+    cube.style.setProperty('--z', z)
+
+    for (let x = -3; x <= 3; x++) {}
+  })
 }
