e24d9d3350f08371d6266d1df722da3732b018d8
Nimira43 <n.rabone72@aol.com>
2025-09-22 09:36:01 +0100

    Comparisons..... and hmmmm - not correct

--- Perl/01-introduction/Intro.pl
+++ Perl/01-introduction/Intro.pl
@@ -28,7 +28,26 @@ my $num2 = 25;
 print $num1 + $num2 . "\n";
 print("The sum of $num2 plus $num1 is " . ($num1 + $num2) . "\n");
 
+print("Coercion\n");
 my $string_number = "85";
 print $num1 + $string_number . "\n";
+
 my $string1 = "Orange";
+my $string2 = "Apple";
+my $string3 = "Orange";
 # print $num1 + $string1 . "\n";
+
+print("Maths Operations\n");
+print $num1 + $num2 . "\n";
+print $num1 / $num2 . "\n";
+print $num1 - $num2 . "\n";
+print $num1 * $num2 . "\n";
+
+print("Comparisons\n");
+print $num1 > $num2 . "\n";
+print $num1 < $num2 . "\n";
+print $num2 == $num1 . "\n";
+print $num1 % $num2 . "\n";
+print $string1 == $string1 . "\n";
+print $string1 == $string2 . "\n";
+print $string1 == $string3 . "\n";
