2d5a754bb3bf546c345a7346071ec26f8416c392
Nimira43 <n.rabone72@aol.com>
2025-09-22 10:29:59 +0100

    Name, engine, drivers, championships

--- Perl/01-introduction/formula_one.pl
+++ Perl/01-introduction/formula_one.pl
@@ -2,3 +2,13 @@
 
 use strict;
 use warnings;
+
+my %Formula1Team = (
+  name    => "McLaren",
+  engine  => "Mercedes",
+  drivers => ["Lando Norris", "Oscar Piastri"],
+  championships => {
+    drivers => [1974, 1976, 1984, 1985, 1986, 1988, 1989, 1990, 1991, 1998, 1999, 2008],
+    constructors =>[1974, 1984, 1985, 1988, 1989, 1990, 1991, 1998, 2024]
+  }
+)
