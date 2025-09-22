82c97760d5a2706097ce298531537f0b55b0b766
Nimira43 <n.rabone72@aol.com>
2025-09-22 08:55:03 +0100

    Scalar variables and concat / calculate

--- Perl/01-introduction/Intro.pl
+++ Perl/01-introduction/Intro.pl
@@ -4,9 +4,10 @@ use strict;
 use warnings;
 
 my $name = "Perl";
+print("Hello to the $name World!\n");
 
-# This is a comment
-
+# Comments
+# this is a comment
 =begin
 This 
 is 
@@ -15,11 +16,14 @@ multi
 line 
 comment
 =cut
-
 =pod
 This is another
 multi line
 comment
 =cut
 
-print("Hello to the $name World!");
+# Scalar Variables
+my $num1 = 34;
+my $num2 = 25;
+print $num1 + $num2 . "\n";
+print("The sum of $num2 plus $num1 is " . ($num1 + $num2) . "\n");
