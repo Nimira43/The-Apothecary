18459c789abf9e0acc29ef7738bd5a2235284c4f
Nimira43 <n.rabone72@aol.com>
2025-09-22 08:41:15 +0100

    Comments

--- Perl/01-introduction/Intro.pl
+++ Perl/01-introduction/Intro.pl
@@ -5,4 +5,21 @@ use warnings;
 
 my $name = "Perl";
 
+# This is a comment
+
+=begin
+This 
+is 
+a 
+multi 
+line 
+comment
+=cut
+
+=pod
+This is another
+multi line
+comment
+=cut
+
 print("Hello to the $name World!");
