dc3fc85957acaf9926b2f77c768816e3cabd85f9
Nimira43 <n.rabone72@aol.com>
2025-09-22 11:04:44 +0100

    Completed a complex object (hash) of hashes and arrays

--- Perl/01-introduction/formula_one.pl
+++ Perl/01-introduction/formula_one.pl
@@ -9,6 +9,88 @@ my %Formula1Team = (
   drivers => ["Lando Norris", "Oscar Piastri"],
   championships => {
     drivers => [1974, 1976, 1984, 1985, 1986, 1988, 1989, 1990, 1991, 1998, 1999, 2008],
-    constructors =>[1974, 1984, 1985, 1988, 1989, 1990, 1991, 1998, 2024]
+    constructors =>[1974, 1984, 1985, 1988, 1989, 1990, 1991, 1998, 2024],
+  },
+  stats => {
+    grands_prix => 991,
+    wins => 201,
+    poles => 173,
+    fastest_laps => 182,
+    podiums => 551,
+    points => 7580.5,
+  },
+  championship_partners => {
+    driver => [
+      {
+        name => "Emerson Fittipaldi",
+        nationality => "Brazilian",
+        mclaren_championships => [1974],
+      },
+      {
+        name => "James Hunt",
+        nationality => "British",
+        mclaren_championships => [1976],
+      },
+      {
+        name => "Niki Lauda",
+        nationality => "Austrian",
+        mclaren_championships => [1984],
+      },
+      {
+        name => "Alain Prost",
+        nationality => "French",
+        mclaren_championships => [1985, 1986, 1989],
+      },
+      {
+        name => "Ayrton Senna",
+        nationality => "Brazilian",
+        mclaren_championships => [1988, 1990, 1991],
+      },
+      {
+        name => "Mika Hakkinen",
+        nationality => "Finnish",
+        mclaren_championships => [1998, 1999],
+      },
+      {
+        name => "Lewis Hamilton",
+        nationality => "British",
+        mclaren_championships => [2008],
+      },
+    ],
+    engine => [
+      {
+        name => "Ford",
+        nationality => "American",
+        mclaren_championships => {
+          drivers => [1974, 1976],
+          constructors => [1974],
+        },
+      },
+      {
+        name => "TAG",
+        nationality => "German",
+        mclaren_championships => {
+          drivers => [1984, 1985, 1986],
+          constructors => [1984, 1985],
+        },
+      },
+      {
+        name => "Honda",
+        nationality => "Japanese",
+        mclaren_championships => { 
+          drivers => [1988, 1989, 1990, 1991],
+          constructors => [1988, 1989, 1990, 1991],
+        },
+      },
+      {
+        name => "Mercedes",
+        nationality => "German",
+        mclaren_championships => { 
+          drivers => [1998, 1999, 2008],
+          constructors => [1998, 2024],
+        },
+      },
+    ]
   }
 )
+
