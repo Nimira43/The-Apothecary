510f7a948a2613bc5b9cb41412748df37532b9c5
Nimira43 <n.rabone72@aol.com>
2025-10-23 01:44:25 +0100

    Navbar variables - JS

---
+++ CSS/Vanilla/Doctors-Website/script.js
@@ -0,0 +1,4 @@
+const navbar = document.querySelector('.navbar')
+const navbarOffsetTop = navbar.offsetTop
+const sections = document.querySelectorAll('section')
+const navbarLinks = document.querySelectorAll('.navbar-link')
