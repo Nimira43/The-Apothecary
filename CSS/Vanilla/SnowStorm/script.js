9c7831c693b95a50c80bb5d19503e645e2f7e6f2
Nimira43 <n.rabone72@aol.com>
2025-11-14 02:11:51 +0000

    Setting canvas width and height. Declaring constants

--- CSS/Vanilla/SnowStorm/script.js
+++ CSS/Vanilla/SnowStorm/script.js
@@ -1,2 +1,10 @@
 const canvasEl = document.getElementById('canvas')
 const ctx = document.getElementById('2d')
+
+canvasEl.width = window.innerWidth
+canvasEl.height = window.innerHeight
+
+const starColurs = '#fff'
+const starNumbers = 1000
+const size = 0.005
+const speed = 0.05 
