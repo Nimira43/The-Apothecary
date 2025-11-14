6d4fa20f788683e5dcd931d9d4f33269a033f3fd
Nimira43 <n.rabone72@aol.com>
2025-11-14 02:16:18 +0000

    Setting upmutable variables. Created placeholder for randomSign function

--- CSS/Vanilla/SnowStorm/script.js
+++ CSS/Vanilla/SnowStorm/script.js
@@ -4,7 +4,13 @@ const ctx = document.getElementById('2d')
 canvasEl.width = window.innerWidth
 canvasEl.height = window.innerHeight
 
-const starColurs = '#fff'
-const starNumbers = 1000
+const snowColour = '#fff'
+const snowNumbers = 1000
 const size = 0.005
 const speed = 0.05 
+
+let snow = []
+let snowSpeed = speed * canvasEl.width
+let horizontalVelocity = snowSpeed * randomSign() * Math.random()
+ 
+function randomSign() {}
