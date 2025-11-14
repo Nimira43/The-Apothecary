40592f96b6a31e29f13b5f878f070ef3421e93a6
Nimira43 <n.rabone72@aol.com>
2025-11-14 02:26:54 +0000

    Randomising loop - 1

--- CSS/Vanilla/SnowStorm/script.js
+++ CSS/Vanilla/SnowStorm/script.js
@@ -5,7 +5,7 @@ canvasEl.width = window.innerWidth
 canvasEl.height = window.innerHeight
 
 const snowColour = '#fff'
-const snowNumbers = 1000
+const snowNumber = 1000
 const size = 0.005
 const speed = 0.05 
 
@@ -14,7 +14,9 @@ let snowSpeed = speed * canvasEl.width
 let horizontalVelocity = snowSpeed * randomSign() * Math.random()
 let verticalVelocity = Math.sqrt(Math.pow(snowSpeed, 2) - Math.pow(horizontalVelocity, 2)) * randomSign()
  
-
+for (let i = 0; i < snowNumber; i++) {
+  let speedBoost = Math.random() * 2.5 + 0.5
+}
 
 function randomSign() {
   return Math.random() >= 0.5 ? 1 : -1
