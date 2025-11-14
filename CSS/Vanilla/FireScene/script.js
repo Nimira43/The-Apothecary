7dd98d2bb2c09eb67c11368a82693bcab2117477
Nimira43 <n.rabone72@aol.com>
2025-11-14 04:59:22 +0000

    Made big changes to the JS to make the snow effect be more like embers.  Also made adjustments to the colour of the swing and the amount and size of the embers. Mini-project done.

--- CSS/Vanilla/FireScene/script.js
+++ CSS/Vanilla/FireScene/script.js
@@ -4,24 +4,22 @@ const ctx = canvasEl.getContext('2d')
 canvasEl.width = window.innerWidth
 canvasEl.height = window.innerHeight
 
-const emberColour = '#ffa600ff'   
-const emberNumber = 1000
-const size = 0.008
-const speed = 0.06 
+const emberColour = '#ff8c00'   
+const emberNumber = 300        
+const size = 0.004
+const speed = 0.05 
 
 let embers = []
 let emberSpeed = speed * canvasEl.width
-let horizontalVelocity = emberSpeed * randomSign() * Math.random()
-let verticalVelocity = Math.sqrt(Math.pow(emberSpeed, 2) - Math.pow(horizontalVelocity, 2)) * randomSign()
- 
+
 for (let i = 0; i < emberNumber; i++) {
-  let speedBoost = Math.random() * 2.5 + 0.5
+  let speedBoost = Math.random() * 1.5 + 0.5
   embers[i] = {
     emberRadius: (Math.random() * size * canvasEl.width) / 2,
     horizontalPosition: Math.floor(Math.random() * canvasEl.width),
     verticalPosition: Math.floor(Math.random() * canvasEl.height),
-    horizontalVelocity: horizontalVelocity * speedBoost,
-    verticalVelocity: verticalVelocity * speedBoost 
+    horizontalVelocity: (Math.random() - 0.5) * emberSpeed * 0.2, // gentle sideways drift
+    verticalVelocity: -(Math.random() * emberSpeed * speedBoost) // rising upwards
   }
 }
 
@@ -33,42 +31,35 @@ function runEmbers(timeNow) {
   timeDiff = timeNow - timeLast
   timeLast = timeNow
 
-  ctx.fillStyle = emberColour
-
   for (let i = 0; i < emberNumber; i++) {
     ctx.beginPath()
+    ctx.fillStyle = emberColour
+    ctx.shadowBlur = 30
+    ctx.shadowColor = '#ffbb00ff'
+
     ctx.arc(
       embers[i].horizontalPosition,
       embers[i].verticalPosition,
       embers[i].emberRadius,
-      9,
+      0,
       Math.PI * 2
     )
     ctx.fill()
 
     embers[i].horizontalPosition += embers[i].horizontalVelocity * timeDiff * 0.001
+    embers[i].verticalPosition += embers[i].verticalVelocity * timeDiff * 0.001
 
     if (embers[i].horizontalPosition < 0 - embers[i].emberRadius) {
       embers[i].horizontalPosition = canvasEl.width + embers[i].emberRadius
-    } else if (
-      embers[i].horizontalPosition > canvasEl.width + embers[i].emberRadius
-    ) {
+    } else if (embers[i].horizontalPosition > canvasEl.width + embers[i].emberRadius) {
       embers[i].horizontalPosition = 0 - embers[i].emberRadius
     }
-    
-    embers[i].verticalPosition += embers[i].verticalVelocity * timeDiff * 0.001
 
     if (embers[i].verticalPosition < 0 - embers[i].emberRadius) {
       embers[i].verticalPosition = canvasEl.height + embers[i].emberRadius
-    } else if (
-      embers[i].verticalPosition > canvasEl.height + embers[i].emberRadius
-    ) {
-      embers[i].verticalPosition = 0 - embers[i].emberRadius
+      embers[i].horizontalPosition = Math.random() * canvasEl.width
     }
   }
   requestAnimationFrame(runEmbers)
 }
 
-function randomSign() {
-  return Math.random() >= 0.5 ? 1 : -1
-}
