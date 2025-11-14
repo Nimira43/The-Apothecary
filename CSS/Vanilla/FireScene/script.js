d104b1d868cc1571a4479cf3908192c4f3056dc2
Nimira43 <n.rabone72@aol.com>
2025-11-14 04:09:58 +0000

    Updated the JS -snow replaced by embers

--- CSS/Vanilla/FireScene/script.js
+++ CSS/Vanilla/FireScene/script.js
@@ -4,69 +4,52 @@ const ctx = canvasEl.getContext('2d')
 canvasEl.width = window.innerWidth
 canvasEl.height = window.innerHeight
 
-const snowColour = '#fff'
-const snowNumber = 1000
-const size = 0.008
-const speed = 0.06 
+const emberColour = '#ff4500'  
+const emberNumber = 400         
+const size = 0.01               
+const speed = 0.08              
 
-let snow = []
-let snowSpeed = speed * canvasEl.width
-let horizontalVelocity = snowSpeed * randomSign() * Math.random()
-let verticalVelocity = Math.sqrt(Math.pow(snowSpeed, 2) - Math.pow(horizontalVelocity, 2)) * randomSign()
- 
-for (let i = 0; i < snowNumber; i++) {
+let embers = []
+let emberSpeed = speed * canvasEl.width
+let horizontalVelocity = emberSpeed * randomSign() * Math.random()
+let verticalVelocity = Math.sqrt(Math.pow(emberSpeed, 2) - Math.pow(horizontalVelocity, 2)) * randomSign()
+
+for (let i = 0; i < emberNumber; i++) {
   let speedBoost = Math.random() * 2.5 + 0.5
-  snow[i] = {
-    snowRadius: (Math.random() * size * canvasEl.width) / 2,
-    horizontalPosition: Math.floor(Math.random() * canvasEl.width),
-    verticalPosition: Math.floor(Math.random() * canvasEl.height),
-    horizontalVelocity: horizontalVelocity * speedBoost,
-    verticalVelocity: verticalVelocity * speedBoost 
+  embers[i] = {
+    radius: (Math.random() * size * canvasEl.width) / 2,
+    x: Math.floor(Math.random() * canvasEl.width),
+    y: Math.floor(Math.random() * canvasEl.height),
+    vx: horizontalVelocity * speedBoost,
+    vy: verticalVelocity * speedBoost 
   }
 }
 
 let timeDiff, timeLast = 0
-requestAnimationFrame(runSnow)
+requestAnimationFrame(runEmbers)
 
-function runSnow(timeNow) {
+function runEmbers(timeNow) {
   ctx.clearRect(0, 0, canvasEl.width, canvasEl.height)
   timeDiff = timeNow - timeLast
   timeLast = timeNow
 
-  ctx.fillStyle = snowColour
+  ctx.fillStyle = emberColour
 
-  for (let i = 0; i < snowNumber; i++) {
+  for (let i = 0; i < emberNumber; i++) {
     ctx.beginPath()
-    ctx.arc(
-      snow[i].horizontalPosition,
-      snow[i].verticalPosition,
-      snow[i].snowRadius,
-      9,
-      Math.PI * 2
-    )
+    ctx.arc(embers[i].x, embers[i].y, embers[i].radius, 0, Math.PI * 2)
     ctx.fill()
 
-    snow[i].horizontalPosition += snow[i].horizontalVelocity * timeDiff * 0.001
+    embers[i].x += embers[i].vx * timeDiff * 0.001
+    embers[i].y += embers[i].vy * timeDiff * 0.001
 
-    if (snow[i].horizontalPosition < 0 - snow[i].snowRadius) {
-      snow[i].horizontalPosition = canvasEl.width + snow[i].snowRadius
-    } else if (
-      snow[i].horizontalPosition > canvasEl.width + snow[i].snowRadius
-    ) {
-      snow[i].horizontalPosition = 0 - snow[i].snowRadius
-    }
-    
-    snow[i].verticalPosition += snow[i].verticalVelocity * timeDiff * 0.001
+    if (embers[i].x < 0 - embers[i].radius) embers[i].x = canvasEl.width + embers[i].radius
+    else if (embers[i].x > canvasEl.width + embers[i].radius) embers[i].x = 0 - embers[i].radius
 
-    if (snow[i].verticalPosition < 0 - snow[i].snowRadius) {
-      snow[i].verticalPosition = canvasEl.height + snow[i].snowRadius
-    } else if (
-      snow[i].verticalPosition > canvasEl.height + snow[i].snowRadius
-    ) {
-      snow[i].verticalPosition = 0 - snow[i].snowRadius
-    }
+    if (embers[i].y < 0 - embers[i].radius) embers[i].y = canvasEl.height + embers[i].radius
+    else if (embers[i].y > canvasEl.height + embers[i].radius) embers[i].y = 0 - embers[i].radius
   }
-  requestAnimationFrame(runSnow)
+  requestAnimationFrame(runEmbers)
 }
 
 function randomSign() {
