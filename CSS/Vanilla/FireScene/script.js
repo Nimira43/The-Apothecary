9ff3c6860bedcb56af1244651f6a38c967c7dcb2
Nimira43 <n.rabone72@aol.com>
2025-11-14 04:43:08 +0000

    Updating the JS. Small updates made elsewhere.

--- CSS/Vanilla/FireScene/script.js
+++ CSS/Vanilla/FireScene/script.js
@@ -4,24 +4,24 @@ const ctx = canvasEl.getContext('2d')
 canvasEl.width = window.innerWidth
 canvasEl.height = window.innerHeight
 
-const emberColour = '#ff4500'  
-const emberNumber = 400         
-const size = 0.01               
-const speed = 0.08              
+const emberColour = '#ffa600ff'   
+const emberNumber = 1000
+const size = 0.008
+const speed = 0.06 
 
 let embers = []
 let emberSpeed = speed * canvasEl.width
 let horizontalVelocity = emberSpeed * randomSign() * Math.random()
 let verticalVelocity = Math.sqrt(Math.pow(emberSpeed, 2) - Math.pow(horizontalVelocity, 2)) * randomSign()
-
+ 
 for (let i = 0; i < emberNumber; i++) {
   let speedBoost = Math.random() * 2.5 + 0.5
   embers[i] = {
-    radius: (Math.random() * size * canvasEl.width) / 2,
-    x: Math.floor(Math.random() * canvasEl.width),
-    y: Math.floor(Math.random() * canvasEl.height),
-    vx: horizontalVelocity * speedBoost,
-    vy: verticalVelocity * speedBoost 
+    emberRadius: (Math.random() * size * canvasEl.width) / 2,
+    horizontalPosition: Math.floor(Math.random() * canvasEl.width),
+    verticalPosition: Math.floor(Math.random() * canvasEl.height),
+    horizontalVelocity: horizontalVelocity * speedBoost,
+    verticalVelocity: verticalVelocity * speedBoost 
   }
 }
 
@@ -37,17 +37,34 @@ function runEmbers(timeNow) {
 
   for (let i = 0; i < emberNumber; i++) {
     ctx.beginPath()
-    ctx.arc(embers[i].x, embers[i].y, embers[i].radius, 0, Math.PI * 2)
+    ctx.arc(
+      embers[i].horizontalPosition,
+      embers[i].verticalPosition,
+      embers[i].emberRadius,
+      9,
+      Math.PI * 2
+    )
     ctx.fill()
 
-    embers[i].x += embers[i].vx * timeDiff * 0.001
-    embers[i].y += embers[i].vy * timeDiff * 0.001
+    embers[i].horizontalPosition += embers[i].horizontalVelocity * timeDiff * 0.001
 
-    if (embers[i].x < 0 - embers[i].radius) embers[i].x = canvasEl.width + embers[i].radius
-    else if (embers[i].x > canvasEl.width + embers[i].radius) embers[i].x = 0 - embers[i].radius
+    if (embers[i].horizontalPosition < 0 - embers[i].emberRadius) {
+      embers[i].horizontalPosition = canvasEl.width + embers[i].emberRadius
+    } else if (
+      embers[i].horizontalPosition > canvasEl.width + embers[i].emberRadius
+    ) {
+      embers[i].horizontalPosition = 0 - embers[i].emberRadius
+    }
+    
+    embers[i].verticalPosition += embers[i].verticalVelocity * timeDiff * 0.001
 
-    if (embers[i].y < 0 - embers[i].radius) embers[i].y = canvasEl.height + embers[i].radius
-    else if (embers[i].y > canvasEl.height + embers[i].radius) embers[i].y = 0 - embers[i].radius
+    if (embers[i].verticalPosition < 0 - embers[i].emberRadius) {
+      embers[i].verticalPosition = canvasEl.height + embers[i].emberRadius
+    } else if (
+      embers[i].verticalPosition > canvasEl.height + embers[i].emberRadius
+    ) {
+      embers[i].verticalPosition = 0 - embers[i].emberRadius
+    }
   }
   requestAnimationFrame(runEmbers)
 }
