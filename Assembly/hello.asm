1039fde3d75581cfccc2a606a85fab11c7f798a3
Nimira43 <n.rabone72@aol.com>
2025-11-16 13:57:36 +0000

    First program

--- Assembly/hello.asm
+++ Assembly/hello.asm
@@ -1,2 +1,16 @@
 section .data
   msg db 'Hello Assembly!', 0Ah ; message with new line
+
+section .text
+  global _start
+
+_start:
+  mov eax, 4        ; syscall: write
+  mov ebx, 1        ; file description: stdout
+  mov ecx, msg      ; pointer to message
+  mov edx, 13       ; length of message
+  int 0x80          ; call kernal
+
+  mov eax, 1        ; syscall: exit
+  xor ebx, ebx      ; exit code 0
+  int 0x80
