c76478c51be0a044faeb63e2e04be01d5203a2ba
Nimira43 <n.rabone72@aol.com>
2025-11-16 17:40:17 +0000

    UEFA nations 1 to 30

--- Components/fifaNations.js
+++ Components/fifaNations.js
@@ -1,6 +1,290 @@
 let uefaNations = [
   {
     id: 1,
+    country: 'Albania'
+  },
+  {
+    id: 2,
+    country: 'Austria'
+  },
+  {
+    id: 3,
+    country: 'Belgium'
+  },
+  {
+    id: 4,
+    country: 'Bulgaria'
+  },
+  {
+    id: 5,
+    country: 'Cyprus'
+  },
+  {
+    id: 6,
+    country: 'Czechia'
+  },
+  {
+    id: 7,
+    country: 'Denmark'
+  },
+  {
+    id: 8,
+    country: 'Republic of Ireland'
+  },
+  {
+    id: 9,
+    country: 'England'
+  },
+  {
+    id: 10,
+    country: 'Finland'
+  },
+  {
+    id: 11,
+    country: 'France'
+  },
+  {
+    id: 12,
+    country: 'Germany'
+  },
+  {
+    id: 13,
+    country: 'Greece'
+  },
+  {
+    id: 14,
+    country: 'Netherlands'
+  },
+  {
+    id: 15,
+    country: 'Hungary'
+  },
+  {
+    id: 16,
+    country: 'Iceland'
+  },
+  {
+    id: 17,
+    country: 'Italy'
+  },
+  {
+    id: 18,
+    country: 'Luxemburg'
+  },
+  {
+    id: 19,
+    country: 'Malta'
+  },
+  {
+    id: 20,
+    country: 'Norway'
+  },
+  {
+    id: 21,
+    country: 'Northern Ireland'
+  },
+  {
+    id: 22,
+    country: 'Poland'
+  },
+  {
+    id: 23,
+    country: 'Portugal'
+  },
+  {
+    id: 24,
+    country: 'Romania'
+  },
+  {
+    id: 25,
+    country: 'Russia'
+  },
+  {
+    id: 27,
+    country: 'Scotland'
+  },
+  {
+    id: 28,
+    country: 'Spain'
+  },
+  {
+    id: 29,
+    country: 'Switzerland'
+  },
+  {
+    id: 30,
+    country: 'Sweden'
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
+    country: ''
+  },
+  {
+    id: 1,
     country: ''
   },
 ]
