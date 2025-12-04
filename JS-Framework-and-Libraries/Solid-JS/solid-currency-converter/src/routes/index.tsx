49146d917e373c07c8e7ae1eb5bca484b960b77d
Nimira43 <n.rabone72@aol.com>
2025-12-04 10:26:29 +0000

    Initial styling.

--- JS-Framework-and-Libraries/Solid-JS/solid-currency-converter/src/routes/index.tsx
+++ JS-Framework-and-Libraries/Solid-JS/solid-currency-converter/src/routes/index.tsx
@@ -2,6 +2,11 @@ function fetchRates() { }
 
 export default function Home() { 
   return (
-    <div>Currency Converter</div>
+    <main class='container centre'>
+      <div class='wrapper'>
+        <h1 class='logo'>Currency Converter</h1>
+      </div>
+    
+    </main>
   )
 }
