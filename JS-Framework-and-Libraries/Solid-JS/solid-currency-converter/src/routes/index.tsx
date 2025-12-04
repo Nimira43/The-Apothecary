2af35c9c932be6322feb410c6aa1c1925828988b
Nimira43 <n.rabone72@aol.com>
2025-12-04 10:31:20 +0000

    Imports

--- JS-Framework-and-Libraries/Solid-JS/solid-currency-converter/src/routes/index.tsx
+++ JS-Framework-and-Libraries/Solid-JS/solid-currency-converter/src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { createSignal, createResource, createMemo } from 'solid-js'
+
 function fetchRates() { }
 
 export default function Home() { 
@@ -5,14 +7,14 @@ export default function Home() {
     <main class='container centre'>
       <div class='wrapper'>
         <h1 class='logo'>Currency Converter</h1>
+        <label>
+          GBP:
+          <input
+            type='number'
+          />
+        </label>
+        <p>USD: $45</p>
       </div>
-      <label>
-        GBP:
-        <input
-          type='number'
-        />
-      </label>
-      <p>USD: $45</p>
     </main>
   )
 }
