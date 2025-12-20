dd08d3beef144d9763fad552e5151f2bd9a194e6
Nimira43 <n.rabone72@aol.com>
2025-12-20 13:06:54 +0000

    typeScale

--- Vanilla/Codex-Viewer/scripts/sections/typography.js
+++ Vanilla/Codex-Viewer/scripts/sections/typography.js
@@ -1,4 +1,10 @@
-
 export const typeScale = {
-  greet: "Hi"
+  "xs":   "0.75rem",   // 12px
+  "sm":   "0.875rem",  // 14px
+  "base": "1rem",      // 16px
+  "lg":   "1.125rem",  // 18px
+  "xl":   "1.25rem",   // 20px
+  "2xl":  "1.5rem",    // 24px
+  "3xl":  "1.875rem",  // 30px
+  "4xl":  "2.25rem"    // 36px
 }
