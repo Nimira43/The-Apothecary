7d97be1d8ad6f066dcfc1f6df9910fb29bab2698
Nimira43 <n.rabone72@aol.com>
2025-12-20 12:44:04 +0000

    colourSection

--- Vanilla/Codex-Viewer/scripts/main.js
+++ Vanilla/Codex-Viewer/scripts/main.js
@@ -1,21 +1,22 @@
 import { colourGroups } from './colours.js' 
 
-const colourSection = document.querySelector('#colours .token-grid')
+const colourSection = document.querySelector('#colours .colour-columns')
 
-Object.entries(colourTokens).forEach(([name, value]) => {
-  const token = document.createElement('div')
-  token.className = 'token'
+Object.entries(colourGroups).forEach(([groupName, tokens]) => {
+  const column = document.createElement('div')
+  column.className = 'colour-column'
+  column.innerHTML = `<h3>${groupName}</h3>`
 
-  token.innerHTML = `
-Object.entries(colourTokens).forEach(([name, value]) => {
-  const token = document.createElement('div')
-  token.className = 'token'
+  Object.entries(tokens).forEach(([name, value]) => {
+    const token = document.createElement('div')
+    token.className = 'token'
 
-  token.innerHTML = `
-    <div class='swatch' style='background: ${value};'></div>
-    <p class='token-name'>${name}</p>
-    <p class='token-value'>${value}</p>
-  `
-})
-  colourGrid.appendChild(token)
+    token.innerHTML = `
+      <div class='swatch' style='background: ${value};'></div>
+      <p class='token-name'>${name}</p>
+      <p class='token-value'>${value}</p>
+    `
+    column.appendChild(token)
+  })
+  colourSection.appendChild(column)
 })
