82b648c0fcd4384f148bf65ded62405485f07a9f
Nimira43 <n.rabone72@aol.com>
2025-12-20 11:48:07 +0000

    colourGrid - 3

--- Vanilla/Codex-Viewer/scripts/main.js
+++ Vanilla/Codex-Viewer/scripts/main.js
@@ -5,4 +5,10 @@ const colourGrid = document.querySelector('#colours .token-grid')
 Object.entries(colourTokens).forEach(([name, value]) => {
   const token = document.createElement('div')
   token.className = 'token'
+
+  token.innerHTML = `
+    <div class='swatch' style='background: ${value};'></div>
+    <p class='token-name'>${name}</p>
+    <p class='token-value'>${value}</p>
+  `
 })
