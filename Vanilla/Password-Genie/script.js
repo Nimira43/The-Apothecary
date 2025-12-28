8ebf9e2f08197775074bd84d6785268982f795ac
Nimira43 <n.rabone72@aol.com>
2025-12-28 08:15:04 +0000

    getRandomLower

--- Vanilla/Password-Genie/script.js
+++ Vanilla/Password-Genie/script.js
@@ -16,7 +16,9 @@ const randomFunction = {
 
 function generatePassword(lower, upper, number, symbol, length) { }
 
-function getRandomLower() {}
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
+}
 function getRandomUpper() {}
 function getRandomNumber() {}
 function getRandomSymbol() {
