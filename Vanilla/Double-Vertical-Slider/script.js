0a768cd74d718dba314b2b4530c99aa1f5a7fad0
Nimira43 <n.rabone72@aol.com>
2025-12-28 21:36:53 +0000

    slidesLength

--- Vanilla/Double-Vertical-Slider/script.js
+++ Vanilla/Double-Vertical-Slider/script.js
@@ -3,3 +3,5 @@ const sliderRight = document.querySelector('.right-slide')
 const sliderLeft = document.querySelector('.slider-left')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
+
+const slidesLength = sliderRight.querySelectorAll('div').length
