dace0f28aabf8dcbca81ad7ecf418bb9dc65bc0d
Nimira43 <n.rabone72@aol.com>
2025-12-28 21:35:44 +0000

    Variables

---
+++ Vanilla/Double-Vertical-Slider/script.js
@@ -0,0 +1,5 @@
+const sliderContainer = document.querySelector('.slider-container')
+const sliderRight = document.querySelector('.right-slide')
+const sliderLeft = document.querySelector('.slider-left')
+const upButton = document.querySelector('.up-button')
+const downButton = document.querySelector('.down-button')
