638d7ca8f0d847c21458a84e85a3f11823d2c8d4
Nimira43 <n.rabone72@aol.com>
2025-12-28 07:26:36 +0000

    Small tweaks. Mini-project completed.

--- Vanilla/Sidebar-Mobile-Menu/script.js
+++ Vanilla/Sidebar-Mobile-Menu/script.js
@@ -3,15 +3,15 @@ const closeBtn = document.querySelector('.close-btn')
 const nav = document.querySelectorAll('.nav')
 
 openBtn
-  .addEventListener('click', () => 
-    nav.forEach(navEl => navEl
+  .addEventListener('click', () => nav
+    .forEach(navEl => navEl
       .classList.add('visible')
     )
   )
 
 closeBtn
-  .addEventListener('click', () => 
-    nav.forEach(navEl => navEl
+  .addEventListener('click', () => nav
+    .forEach(navEl => navEl
       .classList.remove('visible')
     )
   )
